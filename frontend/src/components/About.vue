177d1e85b7164c261b22ee58a339c4982367946a
Martin Insulander <martin@insulander.com>
2024-09-25 09:08:32 +0200

    Show user division if no subdivision Fixes #33

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -13,8 +13,9 @@
         </p>
         <p class="mt-3" v-if="auth.user">
             Logged in as {{ auth.user.personal.name_full }}, {{ auth.user.cid }},
-            {{ auth.user.vatsim.rating.short }} from
-            {{ auth.user.vatsim.subdivision.name }}
+            {{ auth.user.vatsim.rating.short }}
+            <span v-if="auth.user.vatsim.subdivision && auth.user.vatsim.subdivision.name"> from {{ auth.user.vatsim.subdivision.name }}</span>
+            <span v-else-if="auth.user.vatsim.division && auth.user.vatsim.division.name"> from {{ auth.user.vatsim.division.name }}</span>
         </p>
         <p class="mt-3">
             <router-link class="text-grey-darken-1" to="privacy">Privacy policy</router-link>
--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -3,13 +3,27 @@
         <v-progress-circular v-if="auth.pending" indeterminate color="grey" size="60" />
         <div v-else class="text-grey text-center">
             <h1 class="font-weight-thin text-h4">Welcome to VatIRIS</h1>
-            <p class="font-weight-light text-grey-darken-1 mt-2">Add a window from the top menu above</p>
+            <p class="font-weight-light text-grey-darken-1 mt-2">
+                Add a window from the top menu above
+            </p>
             <p v-if="auth.user" class="font-weight-light text-grey-darken-1 mt-2">
                 {{ auth.user.personal.name_full }}, {{ auth.user.cid }},
-                {{ auth.user.vatsim.rating.short }} from {{ auth.user.vatsim.subdivision.name }}
+                {{ auth.user.vatsim.rating.short }}
+                <span
+                    v-if="auth.user.vatsim.subdivision && auth.user.vatsim.subdivision.name"
+                    class="font-weight-light text-grey-darken-1 mt-2"
+                >
+                    from {{ auth.user.vatsim.subdivision.name }}</span
+                >
+                <span
+                    v-else-if="auth.user.vatsim.division && auth.user.vatsim.division.name"
+                    class="font-weight-light text-grey-darken-1 mt-2"
+                >
+                    from {{ auth.user.vatsim.division.name }}</span
+                >
             </p>
             <p v-else class="font-weight-light text-grey-darken-1 mt-2">
-                To make the most of VatIRIS, please<br/>
+                To make the most of VatIRIS, please<br />
                 <v-btn @click="auth.login" variant="text" color="warning">Login with VATSIM</v-btn>
             </p>
         </div>
