1b38951ed9c19a344fd47cd5780e4581a418c640
Martin Insulander <martin@insulander.com>
2024-11-24 19:57:20 +0100

    Ensure windows stay within browser window

--- frontend/src/stores/windows.ts
+++ frontend/src/stores/windows.ts
@@ -12,16 +12,25 @@ export interface WindowLayout {
 }
 
 export const useWindowsStore = defineStore("windows", () => {
-    const layout = reactive({} as {[key: string]: WindowLayout})
+    const layout = reactive({} as { [key: string]: WindowLayout })
     const focusId = ref("")
-    const winbox = reactive({} as {[key: string]: any})
+    const winbox = reactive({} as { [key: string]: any })
     const unmounting = ref(false) // used when Main.vue is unmounting, to ignore close events
 
     if ("layout" in localStorage) Object.assign(layout, JSON.parse(localStorage.layout))
     if ("focusId" in localStorage) focusId.value = localStorage.focusId
 
-    watch(layout, () => localStorage.layout = JSON.stringify(layout))
-    watch(focusId, () => localStorage.focusId = focusId.value)
+    watch(layout, () => (localStorage.layout = JSON.stringify(layout)))
+    watch(focusId, () => (localStorage.focusId = focusId.value))
+    watch(winbox, () => {
+        for (const id in winbox) {
+            const wb = winbox[id]
+            if (wb.x < 0) wb.move(0, wb.y)
+            if (wb.y < 30) wb.move(wb.x, 30)
+            if (wb.x + wb.width > window.innerWidth) wb.resize(window.innerWidth - wb.x, wb.height)
+            if (wb.y + wb.height > window.innerHeight) wb.resize(wb.width, window.innerHeight - wb.y)
+        }
+    })
 
     return { layout, focusId, winbox, unmounting }
 })
