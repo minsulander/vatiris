7bff67914cda5c9c7458f9852e26a7e4f3e63a2d
Martin Insulander <martin@insulander.com>
2025-01-27 22:19:46 +0100

    A bunch of testing trying to figure out how to get info from euroscope

--- euroscope-plugin/src/plugin.h
+++ euroscope-plugin/src/plugin.h
@@ -4,17 +4,32 @@
 #include "EuroScopePlugIn.h"
 #pragma warning(pop)
 
+#include "json.hpp"
 #include <string>
 
 namespace VatIRIS
 {
-    class VatIRISPlugin : public EuroScopePlugIn::CPlugIn
-    {
+class VatIRISPlugin : public EuroScopePlugIn::CPlugIn
+{
     public:
-        VatIRISPlugin();
-        ~VatIRISPlugin();
+    VatIRISPlugin();
+    ~VatIRISPlugin();
 
-        void DisplayMessage(const std::string &message,
-                            const std::string &sender = "VatIRIS");
-    };
-}
+    void OnFlightPlanFlightPlanDataUpdate(EuroScopePlugIn::CFlightPlan FlightPlan);
+    void OnFlightPlanControllerAssignedDataUpdate(EuroScopePlugIn::CFlightPlan FlightPlan, int DataType);
+    void OnFlightPlanDisconnect(EuroScopePlugIn::CFlightPlan FlightPlan);
+    bool OnCompileCommand(const char *commandLine);
+    void OnTimer(int counter);
+
+    void DebugMessage(const std::string &message, const std::string &sender = "VatIRIS");
+    void DisplayMessage(const std::string &message, const std::string &sender = "VatIRIS");
+
+    private:
+    bool FilterFlightPlan(EuroScopePlugIn::CFlightPlan FlightPlan);
+
+    bool updateAll;
+    bool debug;
+    nlohmann::json pendingUpdates;
+    std::time_t lastUpdateTime, lastPostTime;
+};
+} // namespace VatIRIS
---
+++ euroscope-plugin/src/postjson.cpp
@@ -0,0 +1,75 @@
+#include "postjson.h"
+#include <sstream>
+#include <wininet.h>
+
+#pragma comment(lib, "wininet.lib")
+
+namespace VatIRIS
+{
+HANDLE mutex = NULL;
+std::string connectionError;
+
+DWORD WINAPI PostJson(LPVOID lpParameter)
+{
+    std::stringstream err;
+    ThreadData *data = static_cast<ThreadData *>(lpParameter);
+
+    WaitForSingleObject(mutex, INFINITE);
+
+    HINTERNET hInternet = InternetOpen("WinInet JSON POST", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    if (!hInternet) {
+        err << "Failed to open internet: " << GetLastError();
+        connectionError = err.str();
+        ReleaseMutex(mutex);
+        delete data;
+        return 1;
+    }
+
+    HINTERNET hConnect = InternetConnect(hInternet, data->host.c_str(), INTERNET_DEFAULT_HTTPS_PORT,
+                                         NULL, NULL, INTERNET_SERVICE_HTTP, 0, 0);
+    if (!hConnect) {
+        err << "Failed to connect: " << GetLastError();
+        InternetCloseHandle(hInternet);
+        connectionError = err.str();
+        ReleaseMutex(mutex);
+        delete data;
+        return 1;
+    }
+
+    const char *acceptTypes[] = { "application/json", NULL };
+    HINTERNET hRequest = HttpOpenRequest(hConnect, "POST", data->path.c_str(), NULL, NULL,
+                                         acceptTypes, INTERNET_FLAG_SECURE | INTERNET_FLAG_RELOAD, 0);
+    if (!hRequest) {
+        err << "Failed to open request: " << GetLastError();
+        InternetCloseHandle(hConnect);
+        InternetCloseHandle(hInternet);
+        connectionError = err.str();
+        ReleaseMutex(mutex);
+        delete data;
+        return 1;
+    }
+
+    std::string jsonString = data->jsonData.dump();
+    std::string headers = "Content-Type: application/json\r\n";
+
+    if (!HttpSendRequest(hRequest, headers.c_str(), headers.length(), (LPVOID)jsonString.c_str(),
+                         jsonString.length())) {
+        err << "Failed to send request: " << GetLastError();
+        InternetCloseHandle(hRequest);
+        InternetCloseHandle(hConnect);
+        InternetCloseHandle(hInternet);
+        connectionError = err.str();
+        ReleaseMutex(mutex);
+        delete data;
+        return 1;
+    }
+
+    InternetCloseHandle(hRequest);
+    InternetCloseHandle(hConnect);
+    InternetCloseHandle(hInternet);
+    connectionError = "";
+    ReleaseMutex(mutex);
+    delete data;
+    return 0;
+}
+} // namespace VatIRIS
