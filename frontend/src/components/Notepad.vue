27c9ebe6a62541322c358779c03d5481d2cc5fda
Centiljard <89267105+Centiljard@users.noreply.github.com>
2025-05-13 17:03:12 +0200

    Enhanced Notes (rich-text support) Fixes #71

--- frontend/src/components/Notepad.vue
+++ frontend/src/components/Notepad.vue
@@ -5,53 +5,55 @@
                 variant="text"
                 rounded="0"
                 size="small"
-                :color="text.length > 0 ? 'white' : 'grey-lighten-1'"
+                :color="quillDelta.length > 0 ? 'white' : 'grey-lighten-1'"
                 @click="clear"
-                >CLEAR</v-btn
             >
+                CLEAR
+            </v-btn>
         </div>
-        <textarea
-            light
-            v-model="text"
+
+        <QuillEditor
+            ref="editorRef"
+            v-model:content:delta="quillDelta"
+            contentType="delta"
+            @update:content="input"
             style="
                 width: 100%;
-                height: calc(100% - 20px);
+                height: calc(100% - 25px);
                 font-size: 15px;
+                margin: 0;
                 color: black;
                 padding: 2px;
                 background: #ddd;
                 border-color: transparent;
             "
-            spellcheck="false"
-            :placeholder="auth.user ? 'Type here...' : 'Login to save your notes'"
-            :readonly="!auth.user"
-            @input="input"
         />
     </div>
 </template>
 
 <script setup lang="ts">
+import { QuillEditor, Quill } from "@vueup/vue-quill"
+import { ref, watch, onMounted } from "vue"
 import useEventBus from "@/eventbus"
+import "@vueup/vue-quill/dist/vue-quill.snow.css"
 import { useAuthStore } from "@/stores/auth"
-import { onMounted, ref, watch } from "vue"
 
-const text = ref("")
-
+const editorRef = ref(null)
 const auth = useAuthStore()
-
+const quillDelta = ref<any>({ ops: [] })
 let postTimeout: any = undefined
 
 function clear() {
-    text.value = ""
-    input()
+    setEditorContent({ ops: [] })
+    input({ content: { ops: [] } })
 }
 
-function input() {
+async function input(content) {
     if (postTimeout) clearTimeout(postTimeout)
     postTimeout = setTimeout(() => {
-        console.log("Posting content", text.value.length)
+        console.log("Posting content", content.ops.length)
         postTimeout = undefined
-        auth.postUserData("notepad", { text: text.value })
+        auth.postUserData("notepad", { text: JSON.stringify(content) })
     }, 1000)
 }
 
@@ -71,8 +73,22 @@ bus.on("refresh", () => {
     if (auth.user) fetchContent()
 })
 
+function setEditorContent(delta) {
+    const quill = editorRef.value?.getQuill()
+    if (quill) {
+        quill.setContents(delta, "api") 
+    }
+}
+
 async function fetchContent() {
     const data = await auth.fetchUserData("notepad")
-    if (data.text) text.value = data.text
+    try {
+        const delta = data.text ? JSON.parse(data.text) : { ops: [] }
+        setEditorContent(delta)
+    } catch (e) {
+        console.error("Invalid Delta data in DB:", e)
+        setEditorContent({ ops: [] })
+    }
 }
+
 </script>
