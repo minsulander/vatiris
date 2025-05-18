3b096d0bdb6d0cc706eb284c8f9238d9dca6a939
Martin Insulander <martin@insulander.com>
2025-05-18 21:45:11 +0200

    Typing, styling and behavior fixes for enhanced notepad

--- frontend/src/components/Notepad.vue
+++ frontend/src/components/Notepad.vue
@@ -1,6 +1,9 @@
 <template>
     <div style="width: 100%; height: 100%; background: #666; overflow-y: hidden">
-        <div style="height: 100%;  margin-left: -5px; background: #777">
+        <div v-if="!auth.pending && !auth.user" style="height: 100%; background: #ddd; padding: 5px">
+            Login to use notepad
+        </div>
+        <div v-else style="height: 100%; background: #777">
             <div id="notepad-toolbar">
                 <button class="ql-header" value="1"></button>
                 <button class="ql-header" value="2"></button>
@@ -9,23 +12,35 @@
                 <button class="ql-underline"></button>
                 <button class="ql-list" value="bullet"></button>
                 <button class="ql-list" value="ordered"></button>
-                <button class="trashbin" @click="clear"><span>Cl</span></button>
+                <span class="float-right mr-1 pt-1 text-caption text-grey-lighten-2">
+                    <v-btn
+                        variant="text"
+                        rounded="0"
+                        size="small"
+                        style="margin-top: -3px"
+                        :style="{ color: contentLength > 0 ? '' : '#ccc !important' }"
+                        @click="clear"
+                        >CLEAR</v-btn
+                    >
+                </span>
             </div>
+            <v-progress-linear
+                indeterminate
+                color="#33f"
+                height="5"
+                v-if="loading || auth.pending"
+            ></v-progress-linear>
             <QuillEditor
                 ref="editorRef"
                 :toolbar="'#notepad-toolbar'"
-                v-model:content:delta="quillDelta"
                 contentType="delta"
-                @update:content="input"
+                @update:content="updateContent"
                 style="
                     width: 100%;
-                    height: calc(100% - 25px);
+                    height: calc(100% - 22px);
                     font-size: 15px;
                     margin: 0;
-                    color: black;
-                    padding: 2px;
                     background: #ddd;
-                    border-color: #777;
                 "
             />
         </div>
@@ -34,29 +49,39 @@
 
 <script setup lang="ts">
 import { Delta, QuillEditor } from "@vueup/vue-quill"
-import { ref, watch } from "vue"
+import { computed, onMounted, ref, watch } from "vue"
 import useEventBus from "@/eventbus"
 import "@vueup/vue-quill/dist/vue-quill.snow.css"
 import "@/styles/quill.scss"
 import { useAuthStore } from "@/stores/auth"
 
-const editorRef = ref(null)
+const editorRef = ref(null as any)
 const auth = useAuthStore()
-const quillDelta = ref<any>({ ops: [] })
 let postTimeout: any = undefined
+const loading = ref(false)
+
+const contentLength = ref(0)
 
 function clear() {
     setEditorContent({ ops: [] })
     auth.postUserData("notepad", { text: JSON.stringify({ ops: [] }) })
 }
 
-async function input(content) {
-    if (postTimeout) clearTimeout(postTimeout)
-    postTimeout = setTimeout(() => {
-        console.log("Posting content", content.ops.length)
-        postTimeout = undefined
-        auth.postUserData("notepad", { text: JSON.stringify(content) })
-    }, 1000)
+let settingContent = false
+
+async function updateContent(content: Delta) {
+    const quill = editorRef.value?.getQuill()
+    if (!quill) return
+    contentLength.value = quill.getText().trim().length
+    if (settingContent) return
+    if (auth.user) {
+        if (postTimeout) clearTimeout(postTimeout)
+        postTimeout = setTimeout(() => {
+            console.log("Posting notepad content", content.ops.length)
+            postTimeout = undefined
+            auth.postUserData("notepad", { text: JSON.stringify(content) })
+        }, 1000)
+    }
 }
 
 watch(
@@ -66,59 +91,70 @@ watch(
     },
 )
 
+onMounted(() => {
+    if (auth.user) fetchContent()
+})
+
 const bus = useEventBus()
 bus.on("refresh", () => {
     if (auth.user) fetchContent()
 })
 
-function setEditorContent(delta) {
+function setEditorContent(delta: object) {
     const quill = editorRef.value?.getQuill()
     if (quill) {
+        settingContent = true
         quill.setContents(delta, "api")
+        settingContent = false
     }
 }
 
 async function fetchContent() {
-    const data = await auth.fetchUserData("notepad")
+    loading.value = true
     try {
-        const delta = data.text ? JSON.parse(data.text) : { ops: [] }
-        setEditorContent(delta)
-    } catch (e) {
-        let delta: any = { ops: [] }
-        if (typeof data.text === "string") {
-            delta = {
-                ops: [{ insert: data.text + "\n" }]
+        const data = await auth.fetchUserData("notepad")
+        try {
+            const delta = data.text ? JSON.parse(data.text) : { ops: [] }
+            setEditorContent(delta)
+        } catch (e) {
+            let delta: any = { ops: [] }
+            if (typeof data.text === "string") {
+                delta = {
+                    ops: [{ insert: data.text + "\n" }],
+                }
             }
+            setEditorContent(delta)
         }
-        setEditorContent(delta)
+    } finally {
+        loading.value = false
     }
 }
 </script>
 
 <style scoped>
-    .trashbin {
-        border: none;
-        cursor: pointer;
-        height: 22px; 
-        display: flex;
-        align-items: center;
-        
-        justify-content: center;
-    }
-    .trashbin span {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-        font-weight: bold;
-    }
-    .ql-snow.ql-toolbar button svg, .ql-snow .ql-toolbar button svg{
-        color: white !important;
-    }
-    .ql-stroke {
-        stroke: white !important;
-        color: white !important;
-    }
-    
+.trashbin {
+    border: none;
+    cursor: pointer;
+    height: 22px;
+    display: flex;
+    align-items: center;
+
+    justify-content: center;
+}
+.trashbin span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    font-weight: bold;
+}
+.ql-snow.ql-toolbar button svg,
+.ql-snow .ql-toolbar button svg {
+    color: white !important;
+}
+.ql-stroke {
+    stroke: white !important;
+    color: white !important;
+}
 </style>
