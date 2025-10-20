8f82f3940448ceb36d47663df4c2c6762d8247a6
Martin Insulander <martin@insulander.com>
2025-10-20 21:50:48 +0200

    Implement runway suggestions in Metreport from METAR for airports without WX or ATIS

--- frontend/src/components/met/MetreportMETAR.vue
+++ frontend/src/components/met/MetreportMETAR.vue
@@ -27,6 +27,7 @@
 <script setup lang="ts">
 import { useMetarStore } from "@/stores/metar"
 import { useSettingsStore } from "@/stores/settings"
+import { useWindStore } from "@/stores/wind";
 import { useWxStore } from "@/stores/wx"
 import { computed, onMounted, onUnmounted, ref, watch } from "vue"
 
@@ -34,6 +35,7 @@ const props = defineProps<{ id: string }>()
 
 const wx = useWxStore()
 const metarStore = useMetarStore()
+const windStore = useWindStore()
 const settings = useSettingsStore()
 
 const time = computed(() => metarStore.time)
@@ -49,6 +51,10 @@ const formattedMetreport = computed(() => {
                   ? " <div style='display: inline-block; transform: rotate(90deg);'><i class='mdi mdi-play'></i></div>"
                   : " <i class='mdi mdi-play'></i>"
         metreport = metreport.replace('id="qnh-trend">', `'id="qnh-trend">${trend}`)
+        if (metreport.includes("RWY --") && !wx.rwy(props.id)) {
+            const rwyInUse = windStore.getRunwaysInUse(props.id)
+            if (rwyInUse) metreport = metreport.replace("RWY --" + ' '.repeat(rwyInUse.length-2), `RWY <span class="text-grey-darken-1">${rwyInUse}</span>`)
+        }
     }
     return metreport
 })
@@ -59,15 +65,18 @@ const outdated = ref(false)
 
 let wxSubscription = ""
 let metarSubscription = ""
+let windSubscription = ""
 
 onMounted(() => {
     wxSubscription = wx.subscribe(props.id)
     metarSubscription = metarStore.subscribe(props.id)
+    windSubscription = windStore.subscribe(props.id)
 })
 
 onUnmounted(() => {
     if (wxSubscription) wx.unsubscribe(wxSubscription)
     if (metarSubscription) metarStore.unsubscribe(metarSubscription)
+    if (windSubscription) windStore.unsubscribe(windSubscription)
 })
 
 let changeTimeouts: any[] = []
